ccb424ed95b18df4a851ed96a08aec3f97543e0e
Tomislav Cohar <tom@eventure.my>
2020-11-04 18:00:28 +0100

    SystemD service file, trivial installer and uninstaller script

---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# Check privileges
+if [ $(id -u) != 0 ]; then
+    echo "$0 must be run as root, try sudo $0"
+    exit
+fi
+
+# Create hide.me directory
+mkdir -p /opt/hide.me/
+cp hide.me CA.pem hide.me@.service /opt/hide.me
+chmod +x /opt/hide.me/hide.me
+echo "Binary, CA certificate and SystemD service file installed in /opt/hide.me"
+
+# Check for the token
+if [ ! -f /opt/hide.me/accessToken.txt ]; then
+    echo "Hide.me CLI needs to fetch a token. Please, provide your hide.me credentials"
+    cd /opt/hide.me
+    ./hide.me token any.hideservers.net
+else
+    echo "Reusing token in /opt/hide.me/accessToken.txt"
+fi
+
+# Check for systemctl
+_=$(which systemctl)
+if [ $? -eq 0 ]; then
+    echo "Using SystemD"
+    systemctl link /opt/hide.me/hide.me@.service
+    echo "In order to set up and start a connection execute:"
+    echo "  systemctl enable hide.me@SERVER"
+    echo "  systemctl start hide.me@SERVER"
+    echo "Where SERVER is a server name ( e.g. amsterdam-1 ) or a region ( e.g. nl )"
+fi
+
+echo "Finished"
