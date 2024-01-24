e69c65f888bcd1fad35dba427cc25fc6e596712d
Tomislav Cohar <tom@eventure.my>
2024-01-24 15:28:13 +0100

    Use seeks and truncates when updating resolv.conf

--- wireguard/dns.go
+++ wireguard/dns.go
@@ -1,82 +1,50 @@
 package wireguard
 
 import (
+	"golang.org/x/sys/unix"
 	"io"
 	"log"
 	"net"
 	"os"
 )
 
-// in a container, /etc/resolv.conf cannot be truncated from the
-// beginning as per os.WriteFile, can't be renamed'd etc. but it is writable so
-// try to overwrite its contents nicely.
-func replaceResolveConf(conf string) (err error) {
-	rConfF, err := os.Open("/etc/resolv.conf")
-	if err != nil {
-		return err
-	}
-	rConfF.Seek(0, io.SeekStart)
-	rConfF.WriteString(conf)
-	// if the current resolv.conf is long, this may result in corrupted junk at the end.
-	// Look into a way to write nulls or empty spaces or something from the end of the data from nameServers to
-	// the end of the file, I guess?
-
-	return nil
-}
-
 // Update the system-wide DNS
-func (l *Link) dnsSet(addrs []net.IP) (err error) {
-	if l.resolvConf, err = os.ReadFile("/etc/resolv.conf"); err != nil {
-		log.Println("Link: [ERR] Read /etc/resolv.conf failed")
-		return
-	}
-	if len(l.Config.ResolvConfBackupFile) > 0 { // Backup old resolv.conf if configured to do so
-		switch err = os.WriteFile(l.Config.ResolvConfBackupFile, l.resolvConf, 0644); err {
-		case nil:
-			log.Println("Link: resolv.conf backup in", l.Config.ResolvConfBackupFile)
-		default:
-			log.Println("Link: [WARN] resolv.conf backup to", l.Config.ResolvConfBackupFile, "failed:", err.Error()) // Backup may fail as the contents of the original resolv.conf are kept in l.resolvConf
+func (l *Link) dnsSet( addrs []net.IP ) ( err error ) {
+	file, err := os.OpenFile( "/etc/resolv.conf", os.O_RDWR, 0644 )																						// Open /etc/resolv.conf
+	if err != nil { log.Println( "Link: [ERR] Open /etc/resolv.conf failed" ); return }
+	if l.resolvConf, err = io.ReadAll( file ); err != nil { log.Println( "Link: [ERR] Read /etc/resolv.conf failed" ); return }
+	if len( l.Config.ResolvConfBackupFile ) > 0 {																										// Backup old resolv.conf if configured to do so
+		switch err = os.WriteFile( l.Config.ResolvConfBackupFile, l.resolvConf, 0644 ); err {
+			case nil: log.Println( "Link: resolv.conf backup in", l.Config.ResolvConfBackupFile )
+			default:  log.Println( "Link: [WARN] resolv.conf backup to", l.Config.ResolvConfBackupFile, "failed:", err.Error() )						// Backup may fai. The contents of the original resolv.conf are kept in l.resolvConf and can be restored
 		}
 	}
 
-	nameServers := "timeout 1\n" // Create new content
-	for _, addr := range addrs {
-		nameServers += "nameserver " + addr.String() + "\n"
-	}
+	nameServers := "timeout 1\n"																														// Create new content
+	for _, addr := range addrs { nameServers += "nameserver " + addr.String() + "\n" }
+	
+	if _, err = file.Seek( 0, unix.SEEK_SET ); err != nil { log.Println( "Link: [ERR] Seek in /etc/resolv.conf failed" ); return }						// Seek to start
+	if _, err = file.WriteString( nameServers ); err != nil { log.Println( "Link: [ERR] /etc/resolv.conf update failed" ); return }						// Update
+	if err = file.Truncate( int64( len( nameServers ) ) ); err != nil { log.Println( "Link: [WARN] Truncate /etc/resolv.conf failed" ) }				// Truncate resolv.conf
+	log.Println( "Link: /etc/resolv.conf updated" )
 
-	switch err = os.WriteFile("/etc/resolv.conf", []byte(nameServers), 0644); err { // Update /etc/resolv.conf
-	case nil:
-		log.Println("Link: /etc/resolv.conf updated")
-	default:
-		// First try an alternative, in case we're in a container
-		log.Println("Link: [WARN] /etc/resolv.conf update failed trying replacing content (maybe we're in a container?)")
-		err = replaceResolveConf(nameServers)
-		if err != nil {
-			log.Println("Link: [ERR] /etc/resolv.conf update failed:", err)
-		}
-		l.resolvConf = nil // Don't try to restore, it'll just fail. This is a container so assume eventually it'll be cleaned up
-	}
 	return
 }
 
-func (l *Link) dnsRestore() (err error) {
-	if l.resolvConf == nil {
-		return
-	} // No backup taken
+func (l *Link) dnsRestore() ( err error ) {
+	if l.resolvConf == nil { return }																													// No backup taken
 
-	switch err = os.WriteFile("/etc/resolv.conf", l.resolvConf, 0644); err {
-	case nil:
-		log.Println("Link: /etc/resolv.conf restored")
-	default:
-		log.Println("Link: [ERR] resolv.conf restore failed:", err.Error())
-	}
+	file, err := os.OpenFile( "/etc/resolv.conf", os.O_RDWR, 0644 )																						// Open /etc/resolv.conf
+	if err != nil { log.Println( "Link: [ERR] Open /etc/resolv.conf failed" ); return }
+	if _, err = file.Seek( 0, unix.SEEK_SET ); err != nil { log.Println( "Link: [ERR] Seek in /etc/resolv.conf failed" ); return }						// Seek to start
+	if _, err = file.Write( l.resolvConf ); err != nil { log.Println( "Link: [WARN] /etc/resolv.conf restore failed" ); return }						// Update
+	if err = file.Truncate( int64( len( l.resolvConf ) ) ); err != nil { log.Println( "Link: [WARN] Truncate /etc/resolv.conf failed" ) }				// Truncate resolv.conf
+	log.Println( "Link: /etc/resolv.conf restored" )
 
-	if len(l.Config.ResolvConfBackupFile) > 0 {
-		switch err = os.Remove(l.Config.ResolvConfBackupFile); err {
-		case nil:
-			log.Println("Link: resolv.conf backup in", l.Config.ResolvConfBackupFile, "removed")
-		default:
-			log.Println("Link: [ERR] Removal of", l.Config.ResolvConfBackupFile, "failed, ", err.Error())
+	if len( l.Config.ResolvConfBackupFile ) > 0 {
+		switch err = os.Remove( l.Config.ResolvConfBackupFile ); err {
+			case nil: log.Println( "Link: resolv.conf backup in", l.Config.ResolvConfBackupFile, "removed" )
+			default:  log.Println( "Link: [ERR] Removal of", l.Config.ResolvConfBackupFile, "failed, ", err.Error() )
 		}
 	}
 	return
