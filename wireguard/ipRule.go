ed4a2c995f85ff451561666f53d403361817ce37
Tomislav Cohar <tom@eventure.my>
2025-08-27 17:23:45 +0200

    IPv6 rule fix

--- wireguard/ipRule.go
+++ wireguard/ipRule.go
@@ -22,8 +22,8 @@ func (l *Link) RulesAdd() ( err error ) {
 		l.rule6.Priority = l.Config.RPDBPriority
 		l.rule6.Family = netlink.FAMILY_V6
 		l.rule6.Table = l.Config.RoutingTable
-		l.rule.Mark = uint32(l.Config.Mark)						// mark is zero - route all traffic to this routing table, make exceptions by installing throw routes
-		if l.Config.Mark > 0 { l.rule.Invert = true }			// mark is set  - skip this routing table for marked traffic, route all other traffic to this table
+		l.rule6.Mark = uint32(l.Config.Mark)					// mark is zero - route all traffic to this routing table, make exceptions by installing throw routes
+		if l.Config.Mark > 0 { l.rule6.Invert = true }			// mark is set  - skip this routing table for marked traffic, route all other traffic to this table
 		if err = netlink.RuleAdd( l.rule6 ); err != nil { log.Println( "Link: [ERR] IPv6 RPDB rule addition failed:", err ); return }
 		log.Println( "Link: IPv6 RPDB rule added" )
 	}
@@ -37,5 +37,4 @@ func (l *Link) RulesDel() {
 	if l.rule6 != nil {
 		if err := netlink.RuleDel( l.rule6 ); err == nil { log.Println("Link: IPv6 RPDB rule removed" ) } else { log.Println( "Link: [ERR] IPv6 RPDB rule removal failed:", err ) }
 	}
-	return
 }
